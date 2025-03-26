2851948b137442ac4dd249cc7f544eabc0236722
Cauê Meira <92590617+CaueIwamoto@users.noreply.github.com>
2025-03-26 13:25:40 -0300

    Update main.ino
    Mudança código main backup

--- main/main.ino
+++ main/main.ino
@@ -1,37 +1,52 @@
 #include <LiquidCrystal.h>
 
+//Defs dos botões e buzzer:
 #define BUZZER 13
 #define BOTAO1 10
 #define BOTAO2 9
 #define BOTAO3 8
 #define BOTAO4 7
 #define BOTAO5 6
-//defs dos botoes
 
 
-#define NBUTTONS 5 // numero de botoes
-#define NSONGS 5 // numero de musicas
+#define NBUTTONS 5 //Número dos botões
+#define NSONGS 5 //Números de músicas
 
-uint8_t songsIndex = 0;//index usado para selecionar musicas
+uint8_t songsIndex = 0;//Index usado para selecionar músicas
 
-uint8_t songsList[NSONGS];// ira ficar a lista de musicas
+uint8_t songsList[NSONGS];//Onde irá ficar armazenado a lista de músicas
 
-bool first = 0;//para sair da tela inicial
+bool first = 0;//Para sair da tela inicial
 
-bool songsSelect;//bool para selecao de tela de musicas
+bool songsSelect;//Bool para seleção na tela de músicas
 
-uint8_t nButtons[NBUTTONS] = {BOTAO1, BOTAO2, BOTAO3, BOTAO4, BOTAO5};//instancia dos botoes
+uint8_t nButtons[NBUTTONS] = {BOTAO1, BOTAO2, BOTAO3, BOTAO4, BOTAO5}; //Instância dos botões
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);//instancia lcd
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); //Instância LCD
+//Definindo função tempoInicial para a mensagem inicial:
+long tempoInicial = 0;
+bool mostrarBemvindo = true;
 
+//Tela inicial:
 void displayHome() {
-  lcd.setCursor(3, 0);
-  lcd.print("Press To");
-  lcd.setCursor(5, 1);
-  lcd.print("Start");
-}//tela inicial
+  lcd.clear(); //clear para que o loop seja rodado e apareça outra mensagem:
+  lcd.setCursor(3,0);
+  if (mostrarBemvindo) {
+    lcd.print("Bem Vindo!");
+  } else {
+  lcd.setCursor(1,0);
+  lcd.print("Pressione para");
+  lcd.setCursor(4,1);
+  lcd.print("Iniciar");
+  }
+  
+  //mostrarBemvindo:
+  mostrarBemvindo = !mostrarBemvindo;
+      
+}
 
 void selectSong() {
+  //Tela para seleção das músicas:
   lcd.setCursor(2, 0);
   lcd.print("Select Song");
   lcd.setCursor(0, 1);
@@ -42,28 +57,33 @@ void selectSong() {
   lcd.print("Musica");
   lcd.setCursor(11, 1);
   lcd.print(songsIndex);
-}//tela de selecao de musicas
+}
 
 void setup() {
-  lcd.begin(16, 2); //inicia o lcd
+  lcd.begin(16, 2); //Inicia o display LCD
   for (size_t i = 0; i < NBUTTONS; i++) {
-    pinMode(nButtons[i], INPUT); //inicia todos os botoes
+    pinMode(nButtons[i], INPUT); //Inicia todos os botões
   }
-  displayHome(); // seleciona a tela inicial
+  displayHome(); //Seleciona a tela inicial
 }
 
 void loop() {
   if(!first) {
+  //Tempo de loop da mensagem inicial:
+    if (millis() - tempoInicial >= 2000) {
+      tempoInicial = millis();
+      displayHome();
+    }
     for (size_t i = 0; i < NBUTTONS; i++) {
-      bool pressed = digitalRead(nButtons[i]); //leitura de todos os botoes para selecionar a tela de musicas
+      bool pressed = digitalRead(nButtons[i]); //Leitura de todos os botões para selecionar a tela de música
       if (pressed) {
         first = true;
-        lcd.clear(); //clear para selecionar outro display
+        lcd.clear(); //Clear para selicionar próximo display
       }
     }
   } else {
     if (songsSelect) {
-      selectSong();// selecao de display das musicas
+      selectSong(); //Seleção das músicas
     }
   }
 }
