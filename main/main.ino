ff0a7e6006b3d899fc43e2b56f690733fc362967
Wwolfhf <haniel.franco@gmail.com>
2025-03-26 16:34:17 -0300

    alteração das telas, sendo executado um callback function onde a funçao é atualizar a tela selecionada atraves de um indexador de telas, adicionado o funcionamento ja da tela secundaria de seleção de musicas, e tela de musica ja criada, ainda sem nenhuma interação ou interface. leitura dos botoes da tela de selecao de musica tbm sendo realizada apenas falta a tela principal da musica

--- main/main.ino
+++ main/main.ino
@@ -6,34 +6,46 @@
 #define BOTAO3 8
 #define BOTAO4 7
 #define BOTAO5 6
-//defs dos botoes
+// defs dos botoes
 
+#define SCREENHOME 0
+#define SCREENSELECTSONG 1
+#define SCREENSONG 2
 
 #define NBUTTONS 5 // numero de botoes
 #define NSONGS 5 // numero de musicas
 
-uint8_t songsIndex = 0;//index usado para selecionar musicas
+uint8_t songsIndex = 0; // index usado para selecionar musicas
 
-uint8_t songsList[NSONGS];// ira ficar a lista de musicas
+uint8_t screenIndex; // index da selecao de tela
 
-bool first = 0;//para sair da tela inicial
+uint8_t songsList[NSONGS]; // ira ficar a lista de musicas
 
-bool songsSelect;//bool para selecao de tela de musicas
+bool first = 0; // para sair da tela inicial
 
-uint8_t nButtons[NBUTTONS] = {BOTAO1, BOTAO2, BOTAO3, BOTAO4, BOTAO5};//instancia dos botoes
+uint8_t nButtons[NBUTTONS] = {BOTAO1, BOTAO2, BOTAO3, BOTAO4, BOTAO5}; // instancia dos botoes
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);//instancia lcd
+uint8_t lastSongSelected; // armazena o parametro da ultima musica selecionada
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // instancia lcd
 
 void displayHome() {
-  lcd.setCursor(3, 0);
-  lcd.print("Press To");
+  lcd.setCursor(1, 0);
+  lcd.print("Pressione para");
   lcd.setCursor(5, 1);
-  lcd.print("Start");
-}//tela inicial
+  lcd.print("Comecar");
+} // tela inicial
+
+uint8_t updateSelectSong() {
+  lastSongSelected = songsIndex;
+  selectSong();
+  return true;
+} // funçao de atualizar display e parametros assim ele nao é chamado o tempo todo
 
 void selectSong() {
-  lcd.setCursor(2, 0);
-  lcd.print("Select Song");
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Selecione Musica");
   lcd.setCursor(0, 1);
   lcd.print("<");
   lcd.setCursor(15, 1);
@@ -42,28 +54,75 @@ void selectSong() {
   lcd.print("Musica");
   lcd.setCursor(11, 1);
   lcd.print(songsIndex);
-}//tela de selecao de musicas
+} // tela de selecao de musicas
+
+uint8_t updateSongScreen() {
+  songScreen();
+  return true;
+}
+
+void songScreen() {
+  lcd.clear();
+}
+
+void changeScreen(uint8_t (*callback)(), uint8_t screenID) {
+  screenIndex = screenID;//salva a tela selecionada
+  callback();//funçao de chamar a tela
+}
 
 void setup() {
-  lcd.begin(16, 2); //inicia o lcd
+  lcd.begin(16, 2); // inicia o lcd
   for (size_t i = 0; i < NBUTTONS; i++) {
-    pinMode(nButtons[i], INPUT); //inicia todos os botoes
+    pinMode(nButtons[i], INPUT); // inicia todos os botoes
   }
   displayHome(); // seleciona a tela inicial
 }
 
+void readButtonsSelectionScreen() {
+  bool botao1 = digitalRead(BOTAO1);
+  bool botao5 = digitalRead(BOTAO5);
+  bool botao3 = digitalRead(BOTAO3);
+  if (botao1) { // botao de selecionar a musica anterior
+    if (songsIndex <= 0) {
+      songsIndex = 5; // corrige para respeitar o número de músicas
+      delay(200);
+    } else {
+      songsIndex--;
+      delay(200);
+    }
+  } else if (botao5) { // botao de selecionar a musica seguinte
+    if (songsIndex >= NSONGS) {
+      songsIndex = 0;
+      delay(200);
+    } else {
+      songsIndex++;
+      delay(200); // delay para nao rodar rapido demais entre as musicas
+    }
+  } else if (botao3) {
+    lcd.clear();
+    changeScreen(updateSongScreen, SCREENSONG);
+  }
+} // leitura dos botoes da tela de seleção de musica para poder alterar as musicas selecionadas
+
 void loop() {
-  if(!first) {
+  if (!first) {
     for (size_t i = 0; i < NBUTTONS; i++) {
-      bool pressed = digitalRead(nButtons[i]); //leitura de todos os botoes para selecionar a tela de musicas
+      bool pressed = digitalRead(nButtons[i]); // leitura de todos os botoes para selecionar a tela de musicas
       if (pressed) {
         first = true;
-        lcd.clear(); //clear para selecionar outro display
+        lcd.clear(); // clear para selecionar outro display
+        changeScreen(updateSelectSong, SCREENSELECTSONG);
+        delay(200);
       }
     }
   } else {
-    if (songsSelect) {
-      selectSong();// selecao de display das musicas
+    if (screenIndex == SCREENSELECTSONG) {
+      readButtonsSelectionScreen(); // chama a funçao de ler os botoes 
+      if (lastSongSelected != songsIndex) {
+        updateSelectSong(); // funçao de update de display na tela de selecao de musicas
+      }
+    } else if (screenIndex == SCREENSONG) {
+      // lógica para a tela de execução de música (ainda não implementada)
     }
   }
 }
