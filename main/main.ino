248b41c5a96dad8e36e028b6e7f6f08c643ecec2
Cauê Meira <92590617+CaueIwamoto@users.noreply.github.com>
2025-04-04 14:02:48 -0300

    Melhorando uma parte da documentação, nada de mais

--- main/main.ino
+++ main/main.ino
@@ -1,25 +1,26 @@
 #include <LiquidCrystal.h>
 
+//Definindo buzzer e botôes:
 #define BUZZER 13
 #define BOTAO1 6
 #define BOTAO2 5
 #define BOTAO3 4
 #define BOTAO4 3
 #define BOTAO5 2
-// defs dos botoes
 
+//Definindo leds:
 #define LED1 A4  
 #define LED2 A5  
-// defs dos leds
 
+//Definindo telas:
 #define SCREENHOME 0
 #define SCREENSELECTSONG 1
 #define SCREENSONG 2
 #define SCREENLOOSE 3
 #define SCREENPLAYTIME 4
 
-#define NBUTTONS 5 // numero de botoes
-#define NSONGS 5 // numero de musicas
+#define NBUTTONS 5 //Número de botões
+#define NSONGS 5 //Número  de músicas
 
 #define NNOTASM1 17
 
@@ -35,6 +36,7 @@
 
 #define BUTTONSSLTSCREEN 3
 
+//Notas:
 enum notas{
   DO,
   RE,
@@ -51,8 +53,9 @@ uint8_t difficulty = 0;
 
 uint8_t readButtonsSelectScreen [BUTTONSSLTSCREEN] = {BOTAO1, BOTAO3, BOTAO5};
 
-uint16_t notas[NNOTAS] = {1048, 1176, 1320, 1396, 1568, 1760, 1976}; // freq das notas
+uint16_t notas[NNOTAS] = {1048, 1176, 1320, 1396, 1568, 1760, 1976}; //Frequência das notas
 
+//Definindo das notas de cada música:
 uint8_t musica1[NNOTASM1] = {DO, RE, MI, FA, FA, DO, RE, DO, RE, DO, SOL, FA, MI, DO, RE, MI, FA};
 
 uint8_t musica2[NNOTASM2] = {DO, DO, SOL, SOL, MI, MI, FA, MI, RE, DO};
@@ -63,48 +66,51 @@ uint8_t musica4[NNOTASM4] = {MI, MI, RE, DO, FA, FA, MI, RE, SOL, SOL, FA, MI, F
 
 uint8_t musica5[NNOTASM5] = {DO, RE, MI, RE, MI, FA, MI, FA, SOL, RE, MI, FA, RE, MI, DO};
 
-uint8_t* listaMusicas[NSONGS] = {musica1, musica2, musica3, musica4, musica5}; // ponteiro das listas das musicas acima
+uint8_t* listaMusicas[NSONGS] = {musica1, musica2, musica3, musica4, musica5}; //Ponteiros listas das músicas acima
 
-uint8_t listaNNotasMusicas[NSONGS] = {NNOTASM1, NNOTASM2, NNOTASM3, NNOTASM4, NNOTASM5}; // lista de numero de notas de kd musica
+uint8_t listaNNotasMusicas[NSONGS] = {NNOTASM1, NNOTASM2, NNOTASM3, NNOTASM4, NNOTASM5}; //Lista de número de notas de cada música
 
-uint8_t songsIndex = 0; // index usado para selecionar musicas
+uint8_t songsIndex = 0; //Index para selecionar as músicas
 
-uint8_t screenIndex; // index da selecao de tela
+uint8_t screenIndex; //Index seleção de tela
 
-uint8_t songsList[NSONGS]; // ira ficar a lista de musicas
+uint8_t songsList[NSONGS]; //Lista de música
 
-bool first = 0; // para sair da tela inicial
+bool first = 0; //Para sair da tela inicial
 
-uint8_t nButtons[NBUTTONS] = {BOTAO1, BOTAO2, BOTAO3, BOTAO4, BOTAO5}; // instancia dos botoes
+uint8_t nButtons[NBUTTONS] = {BOTAO1, BOTAO2, BOTAO3, BOTAO4, BOTAO5}; //Instância dos botões
 
-uint8_t lastSongSelected; // armazena o parametro da ultima musica selecionada
+uint8_t lastSongSelected; //Armazena o parâmetro da última música selecionada
 
 bool playTime = 0;
 
 uint8_t lastNvidas;
 
-uint8_t currentNoteIndex = 0; // Índice da nota atual
+uint8_t currentNoteIndex = 0; //Índice da nota atual
 
-LiquidCrystal lcd(12, 11, 10, 9, 8, 7); // instancia lcd
+LiquidCrystal lcd(12, 11, 10, 9, 8, 7); //Instância LCD
 
 void changeScreen(uint8_t (*callback)(), uint8_t screenID) {
-  screenIndex = screenID;//salva a tela selecionada
-  callback();//funçao de chamar a tela
+  screenIndex = screenID;//Salva a tela selecionada
+  callback();//Função para chamar a tela
 }
 
+//Tela inicial:
 void displayHome() {
   lcd.setCursor(1, 0);
   lcd.print("Pressione para");
   lcd.setCursor(5, 1);
   lcd.print("Comecar");
-} // tela inicial
+} 
 
+//Função que atualiza display e parâmetros, assim ele não é chamado o tempo todo.
 uint8_t updateSelectSong() {
   lastSongSelected = songsIndex;
   selectSong();
   return true;
-} // funçao de atualizar display e parametros assim ele nao é chamado o tempo todo
+} 
 
+//Tela seleção das músicas:
 void selectSong() {
   lcd.clear();
   lcd.setCursor(0, 0);
@@ -117,32 +123,37 @@ void selectSong() {
   lcd.print("Musica");
   lcd.setCursor(11, 1);
   lcd.print(songsIndex);
-} // tela de selecao de musicas
+} 
 
+//Update da tela de música:
 uint8_t updateSongScreen() {
   songScreen();
   return true;
-} // update de tela de musica
+} 
 
+//Chama tela de derrota ao perder as vidas música:
 uint8_t updateLoose() {
   loose();
   return true;
-} // chama a tela da derrota
+} 
 
+//Tela derrota:
 void loose(){
   lcd.clear();
   lcd.setCursor(2, 0);
   lcd.print("VOCE PERDEU!");
   delay(2000);
   changeScreen(updateSelectSong, SCREENSELECTSONG);
-} // tela da derrota
+}
 
+//Update da tela enquanto a pessoa está tocando para diminuir as vidas ao errar as notas da música:
 uint8_t updatePlayTimeScreen() {
   lastNvidas = nvidas;
   playTimeScreen();
   return true;
-} // update da tela enquanto a pessoa esta tocando para diminuir as vidas caso erre
+} 
 
+//Tela que aparece no display enquanto a pessoa está tocando a música:
 void playTimeScreen() {
   lcd.clear();
   lcd.setCursor(15, 0);
@@ -159,8 +170,9 @@ void playTimeScreen() {
   lcd.print("B4");
   lcd.setCursor(12, 0);
   lcd.print("B5");
-} // tela que aparece no display enquanto a pessoa toca
+} 
 
+//Função para pegar o tamanho da música de acordo com a dificuldade selecionada:
 uint8_t getSizeMusicByDifficult(uint8_t size, uint8_t difficulty) {
   uint8_t sizeOf;
   if (difficulty == 0) {
@@ -171,8 +183,9 @@ uint8_t getSizeMusicByDifficult(uint8_t size, uint8_t difficulty) {
     sizeOf = size;
   }
   return sizeOf;
-} // função para pegar tamanho da musica de acordo com a dificuldade
+} 
 
+//Tela que aparece enquanto o buzzer toca as notas de cada música:
 void songScreen() {
   lcd.clear();
   uint8_t size = getSizeMusicByDifficult(listaNNotasMusicas[songsIndex], difficulty);
@@ -203,16 +216,16 @@ void songScreen() {
     delay(200);
   }
   playTime = true;
-} // tela que aparece enquanto buzzer toca as notas de kd musica
+} 
 
 void setup() {
-  lcd.begin(16, 2); // inicia o lcd
+  lcd.begin(16, 2); //Inicia o display LCD
   for (size_t i = 0; i < NBUTTONS; i++) {
-    pinMode(nButtons[i], INPUT); // inicia todos os botoes
+    pinMode(nButtons[i], INPUT); //Inicia todos os botões
   }
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
-  displayHome(); // seleciona a tela inicial
+  displayHome(); //Seleciona a tela inicial
 }
 
 void readButtonsSongScreen() {
@@ -229,7 +242,7 @@ void readButtonsSongScreen() {
         noTone(BUZZER);
         digitalWrite(LED2, LOW);
         currentNoteIndex++; // Avança para a próxima nota
-        uint8_t size = getSizeMusicByDifficult(listaNNotasMusicas[songsIndex], difficulty); // tananho de acordo com a dificuldade
+        uint8_t size = getSizeMusicByDifficult(listaNNotasMusicas[songsIndex], difficulty); //Tamanho de acordo com a dificuldade
         if (currentNoteIndex >= size) { // caso concluido nNotas
           playTime = false;
           currentNoteIndex = 0;
